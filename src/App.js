47dddd1ba8d202121da1ebd14d2c73f7719dd0db
Kenji Kuramitsu <kura@harukaze.net>
2022-01-30 00:50:58 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import "./styles.css";
+
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+const {
+  Polly,
+  PollyClient,
+  StartSpeechSynthesisTaskCommand,
+} = require("@aws-sdk/client-polly");
+
+// Set the AWS Region.
+const REGION = "ap-northeast-1"; //e.g. "us-east-1"
+// Create an Amazon S3 service client object.
+const pollyClient = new PollyClient({ region: REGION });
+
+// Create the parameters
+var params = {
+  OutputFormat: "mp3",
+  OutputS3BucketName: "arn:aws:s3:::harukaze-polly",
+  Text: "こんにちは",
+  TextType: "text",
+  VoiceId: "Joanna",
+  SampleRate: "22050",
+};
+
+const run = async () => {
+  try {
+    const data = await pollyClient.send(
+      new StartSpeechSynthesisTaskCommand(params)
+    );
+    console.log("Success, audio file added to " + params.OutputS3BucketName);
+  } catch (err) {
+    console.log("Error putting object", err);
+  }
+};
+run();
+
+
+const style = {
+  width: 200,
+  height: 150,
+  border: "1px dotted #888"
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Version 2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp77500952": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
