583cd14713769db5018f70aaedcfc21aa6085e2f
Kenji Kuramitsu <kura@harukaze.net>
2022-01-29 23:03:32 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aEdit <code>src/App.js</code> and save to reload.
+          Version 2
         </p>
         <a
           className="App-link"
